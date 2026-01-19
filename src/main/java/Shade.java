78d87405a12e6ca5bba881fb4d060eaf1cd0a75c
bitxer <24569832+bitxer@users.noreply.github.com>
2026-01-19 19:05:45 +0800

    renamed from duke to shade

---
+++ src/main/java/Shade.java
@@ -0,0 +1,11 @@
+public class Shade {
+    public static void main(String[] args) {
+        String logo = "   _____ _               _      \n" +
+                "  / ____| |             | |     \n" +
+                " | (___ | |__   __ _  __| | ___ \n" +
+                "  \\___ \\| '_ \\ / _` |/ _` |/ _ \\\n" +
+                "  ____) | | | | (_| | (_| |  __/\n" +
+                " |_____/|_| |_|\\__,_|\\__,_|\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
