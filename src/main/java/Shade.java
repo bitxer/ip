f414afae0f319ad5c45d63809afa0a9154e88796
bitxer <24569832+bitxer@users.noreply.github.com>
2026-01-21 20:05:43 +0800

    completed Level-1

--- src/main/java/Shade.java
+++ src/main/java/Shade.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
+/**
+ * Main Shade class
+ */
 public class Shade {
     public static void main(String[] args) {
         String logo = "   _____ _               _      \n" +
@@ -8,6 +13,12 @@ public class Shade {
                 " |_____/|_| |_|\\__,_|\\__,_|\\___|\n";
         System.out.println("Hello! I'm \n" + logo);
         System.out.println("What can I do for you?\n");
+        String str = "";
+        for (Scanner s = new Scanner(System.in);
+             !str.equals("bye");
+             str = s.nextLine()) {
+            System.out.println(str);
+        }
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
