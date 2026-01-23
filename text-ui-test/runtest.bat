3f09a96405595291a1eb5d8318c84aaac90a87ba
bitxer <24569832+bitxer@users.noreply.github.com>
2026-01-23 16:38:31 +0800

    feat: ui testing

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac -cp ../src/main/java/shadebot/ -Xlint:none -d ../bin ../src/main/java/shadebot/commands/*.java ../src/main/java/shadebot/parsers/*.java ../src/main/java/shadebot/tasks/*.java ../src/main/java/shadebot/*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
